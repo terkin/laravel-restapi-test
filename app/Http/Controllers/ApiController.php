14fdefe104e1f71271b845368688744ad32a726a
Dmitriy Gunchenko <terkin.web@gmail.com>
2017-11-26 11:49:43 +0200

    added documentation for basic endpoints

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Models\Device;
 use App\Repositories\DeviceRepository;
 use App\Transformers\ApiJsonResponse;
 use Illuminate\Foundation\Bus\DispatchesJobs;
@@ -12,7 +10,21 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ * @SWG\Swagger(
+ *     basePath="/api",
+ *     host="127.0.0.1:8080",
+ *     schemes={"http"},
+ *     produces={"application/json"},
+ *          @SWG\Info(
+ *              title="Laravel REST API backend for trimming video",
+ *              version="0.1",
+ *              description="If you need to trim some videos then use this rest api.",
+ *              @SWG\Contact(name="Dima Gunchenko",email="terkin.web@gmail.com"),
+ *              @SWG\License(name="Unlicense")
+ *          ),
+ * )
+ */
 class ApiController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiJsonResponse;
@@ -24,13 +36,31 @@ class ApiController extends BaseController
     const ROUTE_NOT_FOUND       = 4001;
 
     protected $deviceToken;
-    protected $stateMachine;
 
     public function __construct(Request $request)
     {
     	$this->deviceToken = $request->header('deviceToken');
     }
 
+	/**
+	 * @SWG\Get(
+	 *     path="/request-token",
+	 *     summary="Register device and returns deviceToken",
+	 *     @SWG\Response(
+	 *          response=200,
+	 *          description="Generate and returns new device token",
+	 *          examples={
+	 *              "application/json": {
+	 *                  "statusCode"=200,
+	 *                  "statusMessage"="OK",
+	 *                  "data"={
+	 *                      "deviceToken"="$2y$10$9PCR/OVAPeilqB4noJGDZOQXZtqnLA0m/OTnCoBbDGML4Ds8lzvUW",
+	 *                  }
+	 *              }
+	 *          }
+	 *      ),
+	 * ),
+	 */
 	public function requestToken(DeviceRepository $repo) {
 		$device = $repo->createToken();
 		return $this->sendResponse(['deviceToken' => $device->getToken()]);
